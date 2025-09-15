b219dd1acb0b0ce310d2e237a08d736a2498460a
taveramex <taveramex@gmail.com>
2025-09-15 10:30:47 +0000

    Deploying to gh-pages from @ taveramex/typescript-playground@728f2ad29492556d4c5d8b0c3a3fa7856f5747c0 🚀

--- src/bases/01-const-let.ts
+++
@@ -1,8 +0,0 @@
-const firstName:string = "Armando";
-const lastName = "Tamales";
-
-let diceNumber = 5;
-diceNumber = 2;
-
-const containsLetterH = lastName.includes('H');
-console.log({containsLetterH, diceNumber, firstName, lastName});
--- src/bases/02-template.string.ts
+++
@@ -1,7 +0,0 @@
-
-const firstName: string = 'Armando';
-const lastName: string = "O'neal";
-
-const fullName= `${firstName} ${lastName} "es el apellido de alguien", es el numero ${1+1}`;
-
-console.log(fullName);
